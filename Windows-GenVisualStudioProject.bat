a61589be2203bdd65f9fd4cff49b3cf18a49acb0
jorolf <jorolf@gmx.de>
2018-06-10 21:55:40 +0200

    symlink instead of copying

--- Windows-GenVisualStudioProject.bat
+++ Windows-GenVisualStudioProject.bat
@@ -11,10 +11,11 @@
 :: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 :: See the License for the specific language governing permissions and
 :: limitations under the License.
-
+@echo off
 cd build
 cmake ../
 mkdir Debug
 mkdir Release
 copy ..\lib\_bin\ Debug
 copy ..\lib\_bin\ Release
+cd ..
