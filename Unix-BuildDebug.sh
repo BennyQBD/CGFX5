3e8c8f291fd88796d342a6fdb3e9b47c7ee2a32d
jorolf <jorolf@gmx.de>
2018-06-10 22:14:16 +0200

    the build directory is gone, scripts need to create it

--- Unix-BuildDebug.sh
+++ Unix-BuildDebug.sh
@@ -14,6 +14,8 @@
 
 #This version of the build script is hardcoded to the debug version so
 #it can be used by the Makefile
+
+mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE="Debug" ../
 make -j 4
--- Windows-GenVisualStudioProject.bat
+++ Windows-GenVisualStudioProject.bat
@@ -12,6 +12,7 @@
 :: See the License for the specific language governing permissions and
 :: limitations under the License.
 @echo off
+mkdir build
 cd build
 cmake ../
 mkdir Debug
